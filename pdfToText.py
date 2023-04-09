5a9a06edf401de115b86d96dae8e3c97022dadcc
Indira Priya Darsini G <95538028+indirapriyadarsini-g@users.noreply.github.com>
2023-04-09 16:02:36 +0530

    Update pdfToText.py
    Updated deprecated elements

--- pdfToText.py
+++ pdfToText.py
@@ -5,14 +5,18 @@ pdf_file = open('ResearchPaper.pdf', 'rb')
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 
 # Extract the text from each page
-text = ''
-for page in pdf_reader.pages:
-    text += page.extract_text()
+text = ""
+for page_num in range(len(pdf_reader.pages)):
+    # Get the text from the current page
+        page = pdf_reader.pages[page_num]
+        page_text = page.extract_text()
+ # Add the text from the current page to the string
+        text += page_text
 
 pdf_file.close()
 
 # Save the text to research_paper.txt
-with open('research_paper.txt', 'w') as f:
+with open('research_paper.txt', 'w',encoding='utf-8') as f:
     f.write(text)
 
 print('Text saved to research_paper.txt')
