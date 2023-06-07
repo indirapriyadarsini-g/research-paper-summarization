a310b33b286144a2cf87596e657382de86c1714c
Indira Priya Darsini G <95538028+indirapriyadarsini-g@users.noreply.github.com>
2023-06-07 12:46:26 +0530

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,30 @@
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from gensim.corpora import Dictionary
+
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def preprocess(text):
+    # Convert text to lowercase
+    text = text.lower()
+
+    # Tokenization
+    tokens = word_tokenize(text)
+
+    # Stopword removal
+    stopwords_list = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stopwords_list]
+
+    # Remove punctuation
+    tokens = [token for token in tokens if token not in string.punctuation]
+
+    # Lemmatization (optional)
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+    return tokens
